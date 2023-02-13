d384f301b3fd5e703a62f58a969a9ada7b3e837a
David Mulholland <david@futr.ai>
2023-02-13 17:31:17 +0000

    text changed

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
       <WrittenLogo />
 
       <button onClick={() => dispatch(consoleLog({ name: "jim" }))}>
-        Deploy to development s3 bucket
+        master updated!!
       </button>
     </div>
   );
