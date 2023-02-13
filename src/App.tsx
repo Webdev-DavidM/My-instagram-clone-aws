72f4f2c9ffe8298d8774d5eb3efd868da6786143
David Mulholland <david@futr.ai>
2023-02-13 17:53:16 +0000

    text changed

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
       <WrittenLogo />
 
       <button onClick={() => dispatch(consoleLog({ name: "jim" }))}>
-        master updated again withg text
+        updated for the develop branch
       </button>
     </div>
   );
